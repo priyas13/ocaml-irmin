9cfe093c01da7c8e80cf297f9ba7d93f5519a414
Swarn Priya <swarnpriya@pal-nat186-129-4.itap.purdue.edu>
2019-02-06 14:05:03 -0500

    delete

--- ppx-ocaml-4.06-irmin-1.4.0/test-data-types/counter/counter.ml
+++
@@ -1,8 +0,0 @@
-module Make = struct 
-type t = int [@@derive versioned]
-
-let inc t x = t + x
-let dec t x = t - x
-
-let merge ~ancestor v1 v2 = ancestor + (v1-ancestor) + (v2-ancestor)
-end [@@derive_versioned]
