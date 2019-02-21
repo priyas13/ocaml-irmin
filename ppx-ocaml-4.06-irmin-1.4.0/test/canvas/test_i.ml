86815cc1fafe6108e39daf04d8824654cc0bf2db
Swarn Priya <swarnpriya@Swarns-MBP.frontierlocal.net>
2019-02-21 16:21:52 -0500

    t

--- ppx-ocaml-4.06-irmin-1.4.0/test/canvas/test_i.ml
+++ ppx-ocaml-4.06-irmin-1.4.0/test/canvas/test_i.ml
@@ -10,7 +10,7 @@ end
 (* Canvas *)
 let _ =
   U.print_header "Canvas";
-let module MkConfig (Vars: sig val root: string end) : ICanvas.Config = struct
+let module MkConfig (Vars: sig val root: string end) : Icanvas.ICanvas.Config = struct
   let root = Vars.root
   let shared = "/tmp/repos/shared.git"
 
@@ -21,7 +21,7 @@ let module MkConfig (Vars: sig val root: string end) : ICanvas.Config = struct
 end in 
 
 let module CInit = MkConfig(struct let root = "/tmp/repos/canvas.git" end) in 
-let module MInit = ICanvas.MakeVersioned(CInit) in 
+let module MInit = Icanvas.ICanvas.MakeVersioned(CInit) in 
 let module M = Canvas.Canvas in 
 let module Vpst = MInit.Vpst in 
 
