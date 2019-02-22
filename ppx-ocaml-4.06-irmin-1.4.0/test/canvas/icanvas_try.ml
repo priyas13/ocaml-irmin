943c442e580ddefa4fdcdd9fc4703b5823f728e7
Swarn Priya <swarnpriya@Swarns-MBP.frontierlocal.net>
2019-02-21 22:23:25 -0500

    t

--- ppx-ocaml-4.06-irmin-1.4.0/test/canvas/icanvas_try.ml
+++ ppx-ocaml-4.06-irmin-1.4.0/test/canvas/icanvas_try.ml
@@ -96,17 +96,29 @@
             let add t v =
               (S.add t v) >>=
                 (fun k -> ((!on_add) k v) >>= (fun _ -> Lwt.return k))
-            let rec add_adt t (a:OM.t) =
-    (add t =<<  (match a with
-       | OM.B a0 -> (match a0 with 
-        | {tl_t;tr_t;bl_t;br_t} -> 
-         (add_adt t tl_t >>= fun tl_t' ->
-          add_adt t tr_t >>= fun tr_t' ->
-          add_adt t bl_t >>= fun bl_t' ->
-          add_adt t br_t >>= fun br_t' ->
-          Lwt.return @@ B {tl_t=tl_t'; tr_t=tr_t'; 
-                           bl_t=bl_t'; br_t=br_t'}))
-        | OM.N a0 -> Lwt.return @@ (N a0)) : K.t Lwt.t)
+       let rec add_adt t (a : Canvas.t) =
+              ((add t) =<<
+                 (match a with
+                  | OM.B a0 ->
+                      (match a0 with
+                       | { tl_t; tr_t; bl_t; br_t;_} ->
+                           (add_adt t tl_t) >>=
+                             ((fun tl_t' ->
+                                 (add_adt t tr_t) >>=
+                                   (fun tr_t' ->
+                                      (add_adt t bl_t) >>=
+                                        (fun bl_t' ->
+                                           (add_adt t br_t) >>=
+                                             (fun br_t' ->
+                                                Lwt.return @@
+                                                  {
+                                                    tl_t = tl_t';
+                                                    tr_t = tr_t';
+                                                    bl_t = bl_t';
+                                                    br_t = br_t'
+                                                  }))))))
+                        >>= ((fun a0' -> Lwt.return @@ (B a0')))
+                  | OM.N {r;g;b} -> Lwt.return @@ (N {r;g;b})) : K.t Lwt.t)
 
   let rec read_adt t (k:K.t) : OM.t Lwt.t =
     find t k >>= fun aop ->
