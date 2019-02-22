51c3dfc50d59e4cc63d9251b0ab229018f1283e0
Swarn Priya <swarnpriya@Swarns-MBP.frontierlocal.net>
2019-02-21 22:49:04 -0500

    t

--- ppx-ocaml-4.06-irmin-1.4.0/test/canvas/icanvas_try.ml
+++ ppx-ocaml-4.06-irmin-1.4.0/test/canvas/icanvas_try.ml
@@ -118,7 +118,8 @@
                                                     br_t = br_t'
                                                   }))))))
                         >>= ((fun a0' -> Lwt.return @@ (B a0')))
-                  | OM.N a0 -> Lwt.return @@ (N a0)) 
+                  | OM.N a0 -> (match a0 with 
+                                |{r;g;b} -> Lwt.return @@ (N a0))) 
 
   let rec read_adt t (k:K.t) : OM.t Lwt.t =
     find t k >>= fun aop ->
